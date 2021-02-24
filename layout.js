30a1e4593f9bc1e28f30837fbf8fe497d41ce845
zargo <jpkz@mail.ru>
2021-02-24 13:25:33 +0600

    не получается hover в фильтре catalog.htm

--- layout.js
+++ layout.js
@@ -1,10 +1,27 @@
-let button = document.getElementById('burger');
+let button = document.querySelector('#menu_burger');
 
-let navigation = document.getElementById('menu');
+let navigation = document.querySelector('.header__navigation');
+let menuClose = document.querySelector('.header__navigation_close');
+let sectionOverlay = document.querySelector('.section__overlay');
 
-let wrp = document.getElementById('wrp');
+navigation.style.display = "none";
 
 button.addEventListener('click', function () {
-    navigation.classList.toggle('display-block');
-    wrp.classList.toggle('display-block');
+    if (navigation.style.display == "none") {
+        if (sectionOverlay)
+            sectionOverlay.style.display = "block";
+        navigation.style.display = "block";
+        navigation.style.zIndex = "1";
+    }
+    else {
+        navigation.style.display = "none";
+        if (sectionOverlay)
+            sectionOverlay.style.display = "none";
+    }
 });
+
+menuClose.addEventListener("click", function () {
+    if (sectionOverlay)
+        sectionOverlay.style.display = "none";
+    navigation.style.display = "none";
+})
